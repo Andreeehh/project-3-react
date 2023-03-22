d943f766e4d2e59f7648bdd22f3e7f3ba4b2078f
André Carvalho <andre.vrlcarvalho@hotmail.com>
2023-03-22 14:18:27 -0300

    segundo hook criado, para lidar com requisições async de url e atualizar a pagina quando seu useRef mudar

--- src/index.js
+++ src/index.js
@@ -4,8 +4,4 @@ import './styles/global-styles.css';
 import { Home } from './templates/home';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <Home />
-  </React.StrictMode>,
-);
+root.render(<Home />);
--- src/templates/home/index.jsx
+++ src/templates/home/index.jsx
@@ -1,176 +1,40 @@
-import { useEffect, useState, useCallback } from 'react';
-
-// import { Component } from "react";
-import './styles.css';
-
-import { loadPosts } from '../../utils/load-post';
-import { Posts } from '../../components/Posts';
-import { Button } from '../../components/Button';
-import { TextInput } from '../../components/TextInput';
-import { act } from 'react-dom/test-utils';
-
-// export class Home extends Component {
-//   state = {
-//     counter: 0,
-//   };
-
-//   handleClick = () => {
-//     this.setState(
-//       (prevState, prevProps) => {
-//         return { counter: prevState.counter + 1 };
-//       },
-//       () => {
-//         console.log(this.state.counter);
-//       }
-//     );
-//     // console.log(this.state.counter)//como o setState é assíncrono, fica diferente os valores, para resolver passar como função de callback
-//   };
-
-//   render() {
-//     return (
-//       <div className="container">
-//         <h1>{this.state.counter}</h1>
-//         <button onClick={this.handleClick}>Increment</button>
-//       </div>
-//     );
-//   }
-// }
+import { useState } from 'react';
+import { useFetch } from './use-fetch';
 
 export const Home = () => {
-  const [posts, setPosts] = useState([]);
-  const [allPosts, setAllPosts] = useState([]);
-  const [page, setPage] = useState(0);
-  const [postsPerPage] = useState(2);
-  const [searchValue, setSearchValue] = useState('');
+  const [postId, setPostId] = useState('');
+  const [result, loading] = useFetch('https://jsonplaceholder.typicode.com/posts/' + postId, {
+    method: 'GET',
+    headers: {
+      abc: '1' + postId,
+    },
+  });
 
-  const filteredPosts = searchValue
-    ? allPosts.filter((post) => {
-        return post.title.toLowerCase().includes(searchValue.toLowerCase());
-      })
-    : posts;
+  if (loading) {
+    return <p>Loading...</p>;
+  }
 
-  const noMorePosts = page + postsPerPage >= allPosts.length;
-
-  const handleLoadPosts = useCallback(async (page, postsPerPage) => {
-    const postsAndPhotos = await loadPosts();
-    setPosts(postsAndPhotos.slice(page, postsPerPage));
-    setAllPosts(postsAndPhotos);
-  }, []);
-
-  useEffect(() => {
-    handleLoadPosts(0, postsPerPage);
-  }, [handleLoadPosts, postsPerPage]);
-
-  const loadMorePosts = () => {
-    const nextPage = page + postsPerPage;
-    const nextPosts = allPosts.slice(nextPage, nextPage + postsPerPage);
-    posts.push(...nextPosts);
-    act(() => {
-      setPosts(posts);
-      setPage(nextPage);
-    });
+  const handleClick = (id) => {
+    setPostId(id);
   };
 
-  const handleChange = (e) => {
-    const { value } = e.target;
-    act(() => {
-      setSearchValue(value);
-    });
-  };
-
-  return (
-    <section className="container">
-      <div className="search-container">
-        {!!searchValue && (
-          <>
-            <h1>Search value: {searchValue}</h1>
-          </>
-        )}
-        <TextInput searchValue={searchValue} handleChange={handleChange} />
-      </div>
-      {filteredPosts.length > 0 && <Posts posts={filteredPosts} />}
-      {filteredPosts.length === 0 && <p>Não existem posts =(</p>}
-
-      <div className="button-container">
-        {!searchValue && ( //se nao tiver busca, exibe o botão
-          <Button text="Load more posts" onClick={loadMorePosts} disabled={noMorePosts} />
+  if (!loading && result) {
+    //123s4667898765456789799004658797546345565456
+    return (
+      <div>
+        {result?.length > 0 ? (
+          result.map((p) => (
+            <div key={`post-${p.id}`} onClick={() => handleClick(p.id)}>
+              <p>{p.title}</p>
+            </div>
+          ))
+        ) : (
+          <div onClick={() => handleClick('')}>
+            <p>{result.title}</p>
+          </div>
         )}
       </div>
-    </section>
-  );
+    );
+  }
+  return <h1>Oi</h1>;
 };
-
-// export class Home2 extends Component {
-//   state = {
-//     posts: [],
-//     allPosts: [],
-//     page: 0,
-//     postPerPage: 10,
-//     searchValue: "",
-//   };
-
-//   async componentDidMount() {
-//     await loadPosts();
-//   }
-
-//   loadPosts = async () => {
-//     const { page, postPerPage } = this.state;
-//     const postsAndPhotos = await loadPosts();
-//     this.setState({
-//       posts: postsAndPhotos.slice(page, postPerPage),
-//       allPosts: postsAndPhotos,
-//     });
-//   };
-
-//   loadMorePosts = () => {
-//     const { page, postPerPage, allPosts, posts } = this.state;
-//     const nextPage = page + postPerPage;
-//     const nextPosts = allPosts.slice(nextPage, nextPage + postPerPage);
-//     posts.push(...nextPosts);
-//     this.setState({ posts, page: nextPage });
-//   };
-
-//   handleChange = (e) => {
-//     const { value } = e.target;
-//     this.setState({ searchValue: value });
-//   };
-
-//   render() {
-//     const { posts, page, postsPerPage, allPosts, searchValue } = this.state;
-//     const noMorePosts = page + postsPerPage >= allPosts.length;
-
-//     const filteredPosts = !!searchValue
-//       ? allPosts.filter((post) => {
-//           return post.title.toLowerCase().includes(searchValue.toLowerCase());
-//         })
-//       : posts;
-
-//     return (
-//       <section className="container">
-//         <div className="search-container">
-//           {!!searchValue && ( //!! transforma searchValue em um booleano, se tiver busca, faça o que estiver dps do &&
-//             <>
-//               <h1>Seach value: {searchValue}</h1>
-//             </>
-//           )}
-//           <TextInput
-//             searchValue={searchValue}
-//             handleChange={this.handleChange}
-//           />
-//         </div>
-//         {filteredPosts.length > 0 && <Posts posts={filteredPosts} />}
-//         {filteredPosts.length === 0 && <p>Não existem posts</p>}
-
-//         <div className="button-container">
-//           {!searchValue && ( //se nao tiver busca, exibe o botão
-//             <Button
-//               text="Load more posts"
-//               onClick={this.loadMorePosts}
-//               disabled={noMorePosts}
-//             />
-//           )}
-//         </div>
-//       </section>
-//     );
-//   }
-// }
